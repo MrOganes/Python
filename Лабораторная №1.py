5d2017262da16ce008b64fedd3a4232d83107c1e
Oganes <elizbarov.oganes2016@gmail.com>
2024-02-09 11:54:37 +0300

    Элизбаров Оганес, группа 37/1, Вариант 3. Задание 1. Функция 3. Задание: найти НОД максимального нечетного непростого делителя числа и произведения цифр данного числа. Комментарий к коду: функция prost(x) проверяет, является ли число простым, функция max_del(x) находит максимальный нечетный непростой делитель числа, функция proizv(number) находит произведение цифр числа, функция NOD(a, b) находит наибольший общий делитель двух чисел.

--- Лабораторная №1.py
+++ Лабораторная №1.py
@@ -26,3 +26,30 @@ def proizv(number):
             proizv*=number%10
         number//=10
     return proizv
+
+#функция 3
+
+def prost(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+def max_del(x):
+    for i in range(x, 0, -1):
+        if(x%i==0 and i%2!=0 and not prost(i)):
+            return i
+    return 1
+
+def proizv(number):
+    proizv = 1
+    while(number):
+        proizv*=number%10
+        number//=10
+    return proizv
+
+def NOD(a,b):
+    for i in range(min(a,b), 0, -1):
+        if(a%i==0 and b%i==0):
+            return i
+    return 1
